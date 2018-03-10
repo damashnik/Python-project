81279eba45a3185c365bb81995681713e8318db5
niko <niko-niko@mailinator.com>
2018-03-10 11:42:30 +0200

    Create Client structure

--- Client/client.py
+++ Client/client.py
@@ -1,1 +1,15 @@
-def 
+def read_config():
+    pass
+
+def add_client_request(server_name):
+    pass
+
+def listen_to_server(server_name,port):
+    pass
+
+def read_status_file():
+    pass
+
+def write_log():
+    pass
+
