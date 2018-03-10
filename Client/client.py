fe6300763b05ee4f5f415c4f279af94d530758c9
niko <niko-niko@mailinator.com>
2018-03-10 21:32:22 +0200

    Added following logic: Log file creation User Menu Add new Client request

--- Client/client.py
+++ Client/client.py
@@ -1,15 +1,96 @@
-def read_config():
+import os
+import time
+import configparser
+import socket
+
+config_file = "client.cfg"
+
+
+def delete_client_request():
     pass
 
-def add_client_request(server_name):
-    pass
+
+def add_client_request():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.connect((server_ip, int(port)))
+        server.send(id)
+        log_line = "Sending request to add client with ID:"+client_id+", to server "+server_ip+" on port "+port+"\n"
+        write_log(log_line)
+        data = server.recv(1024)
+        log_line = "Response from Server" + data + "\n"
+        write_log(log_line)
+
 
 def listen_to_server(server_name,port):
     pass
 
+
 def read_status_file():
     pass
 
-def write_log():
-    pass
+def write_log(line):
+    '''
+    In this function we will care about all events in the system
+    :param log_file: 
+    :return: 
+    
+    '''
+#    if not os.path.exists(log_file):
+#        os.mknod(log_file)
+    localtime = time.asctime(time.localtime(time.time()))
+    try:
+        with open(log_file, 'a+') as file:
+            file.write(localtime+":"+line)
+
+    except IOError as e:
+        print("Unable to open file", e)  # Does not exist OR no read permissions
+
+
+def user_menu():
+    menu = {}
+    menu['1'] = "Add Client."
+    menu['2'] = "Delete Client."
+    menu['3'] = "Run Client."
+    menu['4'] = "Exit."
+    while True:
+        options = menu.keys()
+        for entry in options:
+            print (entry, menu[entry])
+
+        selection = input("Please Select:")
+        print(selection)
+        if selection == '1':
+            add_client_request()
+        elif selection == '2':
+            delete_client_request()
+        elif selection == '3':
+            listen_to_server()
+        elif selection == '4':
+            break
+        else:
+            print ("Unknown Option Selected!")
+
+
+if __name__ == "__main__":
+
+    '''
+    Section of initiation parameters based on configuration file
+    Configuration file client.cfg, should be located in the same directory.
+    Using configparser module for config file parsing
+    '''
+
+    try:
+        config = configparser.ConfigParser()
+        config.read(config_file)
+        server_ip = config['Server']['server_address']
+        port = config['Server']['port']
+        client_id = config['Client']['id']
+        log_file = config['Client']['log_file']
+        log_line = "Initiation: Client "+client_id+" working with Server "+server_ip+" and listening on port "+port+"\n"
+        write_log(log_line)
+        user_menu()
+    except configparser.Error as err:
+        print("Error reading configuration", err)
+        exit()
+
 
