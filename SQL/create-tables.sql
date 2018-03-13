1b6d0c2a6c44653964f6adfce6f665b537747519
damashnik <niko-niko@mailinator.com>
2018-03-13 18:37:12 +0200

    Add SQLite create tables

--- SQL/create-tables.sql
+++ SQL/create-tables.sql
@@ -2,8 +2,8 @@ CREATE DATABASE IF NOT EXISTS `water.db`;
 USE `water.db`;
 -- clients table
 CREATE TABLE IF NOT EXISTS clients (
-  `clientID` int(10) unsigned PRIMARY KEY,
-  `clientIP` varchar (16) NOT NULL,
+  `clientID` int(10)  PRIMARY KEY,
+  `clientIP` varchar(16) NOT NULL,
   `clientKey` varchar(24) NOT NULL,
   `clientName` varchar(50) NOT NULL,
   `datetime` varchar(50) NOT NULL,
@@ -11,12 +11,12 @@ CREATE TABLE IF NOT EXISTS clients (
   );
 -- reports table
 CREATE TABLE IF NOT EXISTS reports (
-  `id` int(10) unsigned PRIMARY KEY,
-  `clientID` int(10) unsigned NOT NULL,
-  `datetime` varchar (16) NOT NULL,
-  `status` int(10) unsigned NOT NULL,
-  `alarm1` int(1) unsigned NOT NULL,
-  `alarm2` int(1) unsigned NOT NULL,
+  `id` int(10)  PRIMARY KEY,
+  `clientID` int(10)  NOT NULL,
+  `datetime` varchar(16) NOT NULL,
+  `status` int(10) NOT NULL,
+  `alarm1` int(1) NOT NULL,
+  `alarm2` int(1) NOT NULL,
   PRIMARY KEY(id)
   FOREIGN KEY (clientID) REFERENCES clients (clientID)
 );
