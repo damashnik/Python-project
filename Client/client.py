d415ae6ba9a8168b2f0bc4c06f3f36b6f66e66e6
damashnik <niko-niko@mailinator.com>
2018-03-15 07:27:30 +0200

    Add CLient key, Client name and changes some structures

--- Client/client.py
+++ Client/client.py
@@ -12,7 +12,8 @@ def delete_client_request():
     pass
 
 def generate_key():
-    key = os.urandom(24).encode('utf-8')
+    key = os.urandom(24).encode('base-64')
+    key.replace('/','_')
     print key
     with open(config_file, "a") as cf:
         cf.write("key = "+key)
