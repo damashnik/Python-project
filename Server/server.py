0773a886bb5804c546b7082684c8813db846ba8b
damashnik <niko-niko@mailinator.com>
2018-03-15 07:43:48 +0200

    Add CLient key, Client name and changes some structures

--- Server/server.py
+++ Server/server.py
@@ -40,9 +40,10 @@ def listen_to_clients():
             data = conn.recv(1024)
             if not data: continue
             #conn.sendall(data)
-            client_id = data[0]
-            client_key = data[1]
-            client_name = data[2]
+            data_tuple=tuple(data.split(",")[:-1])
+            client_id = data_tuple[0]
+            client_key = data_tuple[1]
+            client_name = data_tuple[2]
             values = (client_id, addr[0],client_key,client_name, local_datetime)
             if add_to_db ('clients', values):
                 conn.sendall("Client "+data+" has been added successfully")
