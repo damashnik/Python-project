6363dfd72992b06d2f74f9eb62c7d70cf08512c8
damashnik <niko-niko@mailinator.com>
2018-03-22 21:01:51 +0200

    Added following logic: Added Server side

--- Client/client_actions.py
+++ Client/client_actions.py
@@ -126,9 +126,9 @@ if __name__ == "__main__":
         log_file = config.get('Client','log_file')
         log_line = "Initiation: Client "+client_id+" working with Server "+server_ip+" and listening on port "+port+"\n"
         write_log(log_line)
-        if sys.argc & sys.argv[1]=="a":
+        if len(sys.argv) & sys.argv[1]=="a":
             send_client_request("a")
-        elif sys.argc & sys.argv[1] == "d":
+        elif len(sys.argv) & sys.argv[1] == "d":
             send_client_request("d")
         else:
             user_menu()
